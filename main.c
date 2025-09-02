4d4fc93e074f83798670d70e3b281b806b75a956
commmrade <cigankov807@gmail.com>
2025-09-02 14:40:18 +0300

    Format detection: Audio or Video

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #include <gst/gst.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
 typedef struct State {
     GstElement* pipeline;
@@ -26,34 +27,121 @@ typedef struct State {
 
     GstElement* audio_sink;
     GstElement* video_sink;
+
+    gboolean is_only_audio;
 } State;
+
+
+#define MEDIA_AUDIO 0b00000001
+#define MEDIA_VIDEO 0b00000010
+#define ARRAY_SIZE(arr) sizeof(arr) / sizeof(*arr)
 
 static void pad_added_signal (GstElement *self, GstPad *new_pad, State* state);
 
+static gboolean is_audio_only(const char* filepath);
+
+static gboolean is_audio_only_parse(int *argc, char*** argv, int *error) {
+    static const struct option long_options[] = {
+        {"audio", no_argument, 0, 0},
+        {"video", no_argument, 0, 0},
+        {0, 0, 0, 0}
+    };
+    int option_index;
+
+    int r = getopt_long(*argc, *argv, "av", (const struct option*)&long_options, &option_index);
+    if (r == -1) {
+        *error = -1;
+        return FALSE;
+    }
+
+    int is_audio_only;
+
+    switch (r) {
+        case 0: {
+            const char* name = long_options[option_index].name;
+            if (!strcmp(name, "audio")) {
+                is_audio_only = 1;
+            } else if (!strcmp(name, "video")) {
+                is_audio_only = 0;
+            }
+            break;
+        }
+        case 'a': {
+            is_audio_only = 1;
+            break;
+        } 
+        case 'v': {
+            is_audio_only = 0;
+            break;
+        }
+        default: {
+            is_audio_only = 0;
+            break;
+        }
+    }
+    *error = 0;
+    return is_audio_only;
+}
+
+
 int main(int argc, char** argv) {
     if (argc < 2) {
-        g_print("Usage: ./exec video.file (full path)\n");
+        g_print("Usage: ./exec video.file (full path) [-v (video)| -a (audio)]\n");
         return 1;
     }
 
     State state;
+
+
+    gboolean is_only_audio = is_audio_only(argv[1]);
+    state.is_only_audio = is_only_audio;
+
+    // Cmd arguments must override automatic media type detection
+    if (argc >= 3) {
+        int error;
+        char** argvv = argv + 1;
+        gboolean is_only_audio_result = is_audio_only_parse(&argc, &argvv, &error);
+        if (error == 0) { // no err
+            state.is_only_audio = is_only_audio_result;
+        }
+    }
+    
+
+    printf("Is audio only: %d\n", state.is_only_audio);
+
+    
+    // return 0;
     GstBus* bus = NULL;
     GstMessage* message = NULL;
     
     gst_init(&argc, &argv);
+    printf("Srgfault\n");
 
     state.source = gst_element_factory_make("uridecodebin", "source");
     state.audio_converter = gst_element_factory_make("audioconvert", "audio-converter");
-    state.video_converter = gst_element_factory_make("videoconvert", "video-converter");
     state.audio_resampler = gst_element_factory_make("audioresample", "audio-resampler");
     state.audio_sink = gst_element_factory_make("autoaudiosink", "audio-sink");
-    state.video_sink = gst_element_factory_make("autovideosink", "video-sink");
 
-    if (!state.source || !state.audio_converter || !state.video_converter || !state.audio_resampler || !state.audio_sink || !state.video_sink) {
-        g_printerr("Could not create all elements\n");
-        return -1;
+    
+    if (!state.is_only_audio) {
+        state.video_converter = gst_element_factory_make("videoconvert", "video-converter");
+        state.video_sink = gst_element_factory_make("autovideosink", "video-sink");
+    }
+
+    
+    if (!state.is_only_audio) {
+        if (!state.source || !state.audio_converter || !state.video_converter || !state.audio_resampler || !state.audio_sink || !state.video_sink) {
+            g_printerr("Could not create all elements\n");
+            return -1;
+        }
+    } else {
+        if (!state.source || !state.audio_converter || !state.audio_resampler || !state.audio_sink) {
+            g_printerr("Could not create all elements\n");
+            return -1;
+        }
     }
    
+    
     state.pipeline = gst_pipeline_new("tiktok-pipeline");
     if (!state.pipeline) {
         g_printerr("Could not create a pipeline\n");
@@ -62,12 +150,16 @@ int main(int argc, char** argv) {
 
     const char* prefix_file = "file://";
     char* full_path = malloc(strlen(prefix_file) + strlen(argv[1]) + 1); // +1 for \0
-    // memset(full_path, 0, strlen(prefix_file) + strlen(argv[1]) + 1);
     sprintf(full_path, "%s%s", prefix_file, argv[1]);
-    // Set absolut path
+
+    // Set absolute path
     g_object_set(state.source, "uri", full_path, NULL);
 
-    gst_bin_add_many(GST_BIN(state.pipeline), state.source, state.audio_converter, state.video_converter, state.audio_resampler, state.audio_sink, state.video_sink, NULL);
+    gst_bin_add_many(GST_BIN(state.pipeline), state.source, state.audio_converter, state.audio_resampler, state.audio_sink, NULL);
+    if (!state.is_only_audio) {
+        gst_bin_add_many(GST_BIN(state.pipeline), state.video_converter, state.video_sink, NULL);
+    }
+
     // Link audio stuff
     if (!gst_element_link_many(state.audio_converter, state.audio_resampler, state.audio_sink, NULL)) {
         g_printerr("Unable to link audio elements\n");
@@ -75,10 +167,12 @@ int main(int argc, char** argv) {
         return -1;
     }
     // Link video stuff
-    if (!gst_element_link_many(state.video_converter, state.video_sink, NULL)) {
-        g_printerr("Unable to link audio elements\n");
-        g_object_unref(state.pipeline);
-        return -1;
+    if (!state.is_only_audio) {
+        if (!gst_element_link_many(state.video_converter, state.video_sink, NULL)) {
+            g_printerr("Unable to link audio elements\n");
+            g_object_unref(state.pipeline);
+            return -1;
+        }
     }
 
     // link source to pad added handler
@@ -90,7 +184,6 @@ int main(int argc, char** argv) {
         g_printerr("Was unable to change state\n");
         return -1;
     }
-
     
     gboolean is_running = TRUE;
     bus = gst_element_get_bus(state.pipeline);
@@ -125,7 +218,7 @@ int main(int argc, char** argv) {
 
         gst_message_unref(message);
     } while (is_running);
-    
+
 exit:
     g_object_unref(bus);
     gst_element_set_state(state.pipeline, GST_STATE_NULL);
@@ -154,7 +247,7 @@ static void pad_added_signal (GstElement *self, GstPad *new_pad, State* state) {
         if (GST_PAD_LINK_FAILED(ret)) {
             g_printerr("Could not link audio pad\n");
         }
-    } else if (g_str_has_prefix(new_pad_type, "video/x-raw")) {
+    } else if (!state->is_only_audio && g_str_has_prefix(new_pad_type, "video/x-raw")) {
         converter_sink = gst_element_get_static_pad(state->video_converter, "sink");
 
         if (gst_pad_is_linked(converter_sink)) {
@@ -177,3 +270,34 @@ exit:
         gst_caps_unref(new_pad_caps);
     }
 }   
+
+
+static gboolean is_audio_only(const char* filepath) {
+    const char *audio_exts[] = {
+        ".mp3", ".aac", ".flac", ".wav", ".ogg", ".oga", ".m4a", ".wma", ".opus"
+    };
+    const char *video_exts[] = { // That also includes pictures
+        ".mp4", ".mkv", ".mov", ".avi", ".flv", 
+        ".webm", ".wmv", ".mpeg", ".mpg", ".m2ts", ".png", ".jpeg",
+        ".jpg"
+    };
+
+    gboolean is_audio = FALSE;
+    gboolean is_video = FALSE;
+
+    for (int i = 0; i < ARRAY_SIZE(audio_exts); ++i) {
+        if (g_str_has_suffix(filepath, audio_exts[i])) {
+            is_audio = TRUE;
+            break;
+        }
+    }   
+    
+    for (int i = 0; i < ARRAY_SIZE(audio_exts); ++i) {
+        if (g_str_has_suffix(filepath, video_exts[i])) {
+            is_video = TRUE;
+        }
+    }
+
+    if (is_video) return FALSE;
+    return TRUE;
+}
